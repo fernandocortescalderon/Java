76783c399a2ad657818604ce4df2d6f20d41f498
Fernando Cortés <fernando@cortescalderon.com>
2024-07-11 13:53:42 -0600

    Clase de booleans

---
+++ Boolean.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Boolean {
+    public static void main(String[] args) {
+        /*
+         * boolean varBoolean = true;
+         * System.out.println(varBoolean);
+         * 
+         * if(varBoolean){
+         * System.out.println("La bandera es verdadera");
+         * }
+         * else{
+         * System.out.println("La variable es falsa");
+         * }
+         */
+
+        Scanner consola = new Scanner(System.in);
+
+        System.out.println("¿Cuántos años tienes?");
+        int edad = consola.nextLine();
+        var esAdulto = edad >= 18;
+
+        if (esAdulto) {
+            System.out.println("Eres mayor de edad");
+        } else {
+            System.out.println("Eres menor de edad");
+        }
+    }
+}
