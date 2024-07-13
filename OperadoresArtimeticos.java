f3f2baabda558437847329cd102e3fe97a519310
Fernando Cortés <fernando@cortescalderon.com>
2024-07-13 14:03:20 -0600

    Cambios en formato

--- OperadoresArtimeticos.java
+++ OperadoresArtimeticos.java
@@ -1,6 +1,6 @@
 public class OperadoresArtimeticos {
     public static void main(String[] args) {
-        int a=3, b=2;
+        int a = 3, b = 2;
         var suma = a + b;
         var resta = a - b;
         var multiplicacion = a * b;
@@ -12,15 +12,14 @@ public class OperadoresArtimeticos {
         System.out.println(division);
 
         // El módulo es el residuo entero de la división
-        var modulo = a%b;
+        var modulo = a % b;
         System.out.println(modulo);
 
         // Para ver si un número es par
-        if(a % 2 == 0)
+        if (a % 2 == 0)
             System.out.println("Es número par");
         else
-            System.out.println("Es número impar");    
-
+            System.out.println("Es número impar");
 
     }
 }
