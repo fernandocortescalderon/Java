ed3dadf9a0d130446aa5c9958194495ec05980bb
Fernando Cortés <fernando@cortescalderon.com>
2024-07-13 14:00:38 -0600

    Operadores De Asignacion

---
+++ OperadoresDeAsignacion.java
@@ -0,0 +1,18 @@
+public class OperadoresDeAsignacion {
+    public static void main(String[] args) {
+        int a=3, b=2;
+
+        a += 1;
+        System.out.println(a);
+        a -= 1;
+        System.out.println(a);
+        a *= 2;
+        System.out.println(b);
+        a /= 1;
+        System.out.println(a);
+        a %= 1;
+        System.out.println(a);
+
+
+    }
+}
